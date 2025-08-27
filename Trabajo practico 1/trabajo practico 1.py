951c62c657bec40c73f7500d44d09ba03e32d018
Benja <benjaminarrollo14@gmail.com>
2025-08-27 20:56:59 -0300

    Subo trabajo practico unidad 1

---
+++ Trabajo practico 1/trabajo practico 1.py
@@ -0,0 +1,65 @@
+#Ejercicio uno 
+#print("Hola mundo")
+
+#Ejercicio dos
+# nombre =    input("Ingrese su nombre: ")
+# print(f"Hola  {nombre}!")
+
+#Ejercicio tres
+# nombre = input("Ingrese su nombre: ")
+# apellido = input("Ingrese su apellido: ")
+# edad = input("Ingrese su edad: ")
+# residencia = input("Ingrese su ciudad de residencia: ")
+# print(f"Hola soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.")
+
+#Ejercicio cuatro
+# import math
+# radio = float(input("Ingrese el radio del círculo: "))
+# area = math.pi * (radio ** 2)
+# perimetro = 2 * math.pi * radio
+# print(f"El área del círculo es: {area:.2f}")
+# print(f"El perímetro del círculo es: {perimetro:.2f}")
+
+
+# #Ejercicio cinco
+# segundos = int(input("Ingrese la cantidad de segundos: "))
+# horas = segundos / 3600
+# print(f"La cantidad de segundos ingresadas equivale a: {horas} horas")
+
+#Ejercicio seis
+# num1 = int(input("Ingrese un número: "))
+# print(f"Tabla de multiplicar del {num1}:")
+# for i in range(1, 11):
+#     resultado = num1 * i
+#     print(f"{num1} x {i} = {resultado}")
+
+#Ejercicio siete
+# print("Ingrese dos números distintos a 0 para realizar sus operaciones.")
+# num1 = int(input("Ingrese el primer número: "))
+# num2 = int(input("Ingrese el segundo número: "))
+# suma = num1 + num2
+# resta = num1 - num2
+# multiplicacion = num1 * num2
+# division = num1 / num2
+# print(f"La suma de {num1} y {num2} es: {suma}")
+# print(f"La resta de {num1} y {num2} es: {resta}")
+# print(f"La multiplicación de {num1} y {num2} es: {multiplicacion}")
+# print(f"La división de {num1} y {num2} es: {division:.2f}")
+
+#Ejercicio ocho
+# altura = float(input("Ingrese su altura en metros: "))
+# peso = float(input("Ingrese su peso en kilogramos: "))
+# imc = peso / (altura ** 2)
+# print(f"Su IMC es: {imc:.2f}")
+
+# #Ejercicio nueve
+# temperatura_celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+# temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+# print(f"La temperatura en grados Fahrenheit es: {temperatura_fahrenheit:.2f}°F")
+
+#Ejercicio diez
+# numero = int(input("Ingrese un número: "))
+# numero2 = int(input("Ingrese un segundo número: "))
+# numero3 = int(input("Ingrese otro número: "))
+# promedio = (numero + numero2 + numero3) / 3
+# print(f"El promedio de los números ingresados es: {promedio:.2f}")
